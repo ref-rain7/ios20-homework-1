a8a0dc4bb612ddb7c0c348343a6dc4d7e7bdef9f
zero <zero@smail.nju.edu.cn>
2020-09-24 21:14:09 +0800

    Initial Commit

---
+++ ScreenFlashlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenFlashlight
+//
+//  Created by zero on 2020/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
